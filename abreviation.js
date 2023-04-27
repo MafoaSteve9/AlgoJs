bdfcc6844fc1501b30f76c2112463589eae2b9cd
Mafoa <steve_mafoa@outlook.com>
2023-04-28 01:18:01 +0200

    abreviation done

---
+++ abreviation.js
@@ -0,0 +1,16 @@
+function abreviation(str) {
+    let sub = []
+    let tab = str.split(' ')
+    sub.push(tab);
+    return `${tab[0]} ${tab[1].slice(0,1)}.`
+
+}
+/* Créez un algorithme qui retourne l'abréviation d'un nom et prénom. */
+
+
+/* Tests à passer 🧪 */
+
+console.log(abreviation("John Doe")); // John D.
+console.log(abreviation("Romy Schneider")); // Romy S.
+console.log(abreviation("Alfred Hitchcock")); // Alfred H.
+
