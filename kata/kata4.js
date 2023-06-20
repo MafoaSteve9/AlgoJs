176a25282bae7a643cdc637ca6b6c8e2fe5e2bf7
Mafoa <steve_mafoa@outlook.com>
2023-06-20 11:45:50 +0200

    Removing Elements - Done

--- kata/kata4.js
+++ kata/kata4.js
@@ -1,15 +1,18 @@
 function removeEveryOther(arr){
-    for(let i = 0; i < arr.length; i++){
-         arr.join(' ').split(' ')
-        return arr.splice(i,3)
-
+        
+            const result = arr.filter(item => arr.splice(arr + item, 1));
+            return result;
     }
-}
+    const removeEveryOther = arr => arr.filter(item => arr.splice(arr + item, 1));
 
 
-
-
-
+    // DESCRIPTION:
+    // Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+    
+    // Example:
+    // ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+    
+    // None of the arrays will be empty, so you don't have to worry about that!
 
 console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
 console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
