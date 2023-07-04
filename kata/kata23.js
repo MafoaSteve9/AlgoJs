6cdd5485f1387716dfd5140fc34b92516012c08a
Mafoa <steve_mafoa@outlook.com>
2023-07-04 21:54:36 +0200

    Abreviation Name - Done

---
+++ kata/kata23.js
@@ -0,0 +1,9 @@
+const abbrevName = name => name.split(' ').map(x=>x.slice(0,1)).join('.').toUpperCase();
+
+
+
+console.log(abbrevName("Sam Harris"), "S.H");
+console.log(abbrevName("Patrick Feenan"), "P.F");
+console.log(abbrevName("Evan Cole"), "E.C");
+console.log(abbrevName("P Favuzzi"), "P.F");
+console.log(abbrevName("David Mendieta"), "D.M");
