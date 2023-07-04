afd0408d551b7d47bd118defad4a51ffffb1c6d7
Mafoa <steve_mafoa@outlook.com>
2023-07-04 20:43:25 +0200

    Sum Array - Done - Task : Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ). If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.

---
+++ kata/kata22.js
@@ -0,0 +1,17 @@
+function sumArray(array) {
+    if (array.length < 3) {
+      return 0;
+    }
+  
+    const sortedArray = array.sort((a, b) => a - b);
+    const sum = sortedArray.slice(1, -1).reduce((acc, curr) => acc + curr, 0);
+  
+    return sum;
+  }
+  
+
+console.log(sumArray([ 3, 5 ])                 , 0 );
+console.log(sumArray([ 6, 2, 1, 8, 10 ])       , 16 );
+console.log(sumArray([ 0, 1, 6, 10, 10 ])      , 17 );
+console.log(sumArray([ -6, -20, -1, -10, -12 ]), -28 );
+console.log(sumArray([ -6, 20, -1, 10, -12 ])  , 3 );
