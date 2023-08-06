e5895e542bde838de414243c9650af8a2b843dcf
Mafoa <steve_mafoa@outlook.com>
2023-08-06 22:46:33 +0200

    Order - Done - Task : Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result. Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0). If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.

---
+++ kata/kata30.js
@@ -0,0 +1,9 @@
+function order(words) {
+  const p = words.split(' ');
+  return p.sort((a, b) => {
+    const numA = parseInt(a.match(/\d+/)[0]); // Obtient le premier nombre trouvé dans le mot 'a'
+    const numB = parseInt(b.match(/\d+/)[0]); // Obtient le premier nombre trouvé dans le mot 'b'
+    return numA - numB; // Compare les nombres pour trier les mots
+  }).join(' ');
+}
+console.log(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
