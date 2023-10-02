796e3f87a38105cffe9ad549056a3914544bd5a0
Mafoa <steve_mafoa@outlook.com>
2023-10-02 12:18:51 +0200

    Kata 30 modifié

--- kata/kata30.js
+++ kata/kata30.js
@@ -6,4 +6,5 @@ function order(words) {
     return numA - numB; // Compare les nombres pour trier les mots
   }).join(' ');
 }
+// const order  = words => words.split(' ').sort((a,b) => a.match(/\d+/)[0] - b.match(/\d+/)[0]).join(' ')
 console.log(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
