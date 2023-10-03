3756a8f6b1ae28fab698ee87f2fb3df546e730e3
Mafoa <steve_mafoa@outlook.com>
2023-10-03 18:07:05 +0200

    commentaire modifié

--- kata/kata37.js
+++ kata/kata37.js
@@ -1,11 +1,11 @@
 function stray(numbers) {
-    const tri = numbers.sort((a, b)=> a - b)
-    //Je trie mon tableau pour que mes doublons sois l'un à côté de l'autre.
-    return tri[0] !== tri[1] ? tri[0] : tri[tri.length -1]
-    // Si le premier element est différent du second c'est qu'il est unique donc je le retourne
-    // Sinon je retourne le dernier element du tableau trié car il sera obligatoirement unique
-  }
+    const tri = numbers.sort((a, b) => a - b);
+    // Je trie mon tableau pour que mes doublons sois l'un à coté de l'autre.
 
-console.log(stray([1, 1, 2]), 2);
-console.log(stray([1, 2, 1]), 2);
-console.log(stray([2, 1, 1]), 2);
+
+    return tri[0] !== tri[1] ? tri[0] : tri[tri.length - 1]
+    /* Si le premer élément est différent du du second c'est qu'il est unique
+        donc je le retourne.
+        Sinon je retourne le dernier élément du tableau trié car il sera obligatoirement unique
+    */
+}
